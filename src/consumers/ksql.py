523688c1e1193ed9fdf4818e7af15df578d88161
frei_d <dennis.frei@valiton.com>
2021-02-15 14:47:54 +0100

    feat: Update ksql application

--- src/consumers/ksql.py
+++ src/consumers/ksql.py
@@ -12,22 +12,13 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
-
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
     station_id INTEGER, 
     station_name VARCHAR,
     line VARCHAR
 ) WITH (
-    KAFKA_TOPIC='org.chicago.transit.turnstiles',
+    KAFKA_TOPIC='org.chicago.cta.turnstiles.v1',
     VALUE_FORMAT='AVRO',
     KEY='station_id'
 );
@@ -49,8 +40,8 @@ WITH (
 
 def execute_statement():
     """Executes the KSQL statement against the KSQL API"""
-    # if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
-    #    return
+    if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
+        return
 
     logging.debug("executing ksql statement...")
 
